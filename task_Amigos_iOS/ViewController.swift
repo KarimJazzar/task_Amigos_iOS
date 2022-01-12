fbf404836117cf5ce2e18a39d17f991a2891c764
Karim <c0826750@mylambton.ca>
2022-01-12 02:15:27 +0200

    Initial Commit

---
+++ task_Amigos_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  task_Amigos_iOS
+//
+//  Created by Karim El Jazzar on 12/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
