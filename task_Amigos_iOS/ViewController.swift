05421ec6779a6f2953c934879b7684113d389768
Daniel Miolan <miolan96@gmail.com>
2022-01-20 14:24:52 -0400

    Create complete task view.

--- task_Amigos_iOS/ViewController.swift
+++ task_Amigos_iOS/ViewController.swift
@@ -11,13 +11,14 @@ class ViewController: UIViewController {
     
     var taskList: [Task] = [Task]()
     @IBOutlet weak var incompleteView: UIView!
+    @IBOutlet weak var completeView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        
+        UIView.animate(withDuration: 0, delay: 0, options: .curveEaseOut, animations: {}, completion: { finished in self.completeView.frame.origin.x = (self.completeView.frame.origin.x * 2) + self.completeView.frame.width
+        })
     }
-    
 }
 
