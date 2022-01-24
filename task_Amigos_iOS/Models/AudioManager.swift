e82a4e11f7833b8506b219ca553467d98493c760
simranjit-contact <C0809865@mylambton.ca>
2022-01-25 02:45:24 +0800

    Added AudioManager Class to Models

---
+++ task_Amigos_iOS/Models/AudioManager.swift
@@ -0,0 +1,43 @@
+//
+//  AudioManager.swift
+//  task_Amigos_iOS
+//
+//  Created by user213023 on 1/22/22.
+//
+
+import Foundation
+import AVFoundation
+
+class AudioManager{
+    
+    private var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    //Setter
+    public func setName(name: String) -> Void {
+        self.name = name
+    }
+    //Getter
+    public func getName() -> String {
+        return name
+    }
+    
+    
+    
+    
+    func record(){
+        
+    }
+    
+    func save(){
+        
+    }
+    
+    func playAudio(name: String){
+        
+        
+    }
+    
+}
