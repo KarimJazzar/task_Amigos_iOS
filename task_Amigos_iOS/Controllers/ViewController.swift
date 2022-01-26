e56c13c4221dc832ad30e3a7a1802d7fba24662d
Daniel Miolan <miolan96@gmail.com>
2022-01-26 09:09:12 -0400

    Move core data logic from main controller to task manager.

--- task_Amigos_iOS/Controllers/ViewController.swift
+++ task_Amigos_iOS/Controllers/ViewController.swift
@@ -10,14 +10,11 @@ import CoreData
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        
-        var managedContext: NSManagedObjectContext!
-    
-    var tasks: [Task]?
-    var taskList: [Task] = [Task]()
+
     var incompleteTasks: [Task] = [Task]()
     var completeTasks: [Task] = [Task]()
+    var taskManager: TaskManager = TaskManager()
+    
     private var gestureList: [UISwipeGestureRecognizer.Direction] = [.left, .right]
     
     @IBOutlet weak var incompleteView: UIView!
@@ -28,7 +25,8 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        managedContext = appDelegate.persistentContainer.viewContext
+        incompleteTasks = taskManager.getIncompleteTasksList()
+        completeTasks = taskManager.getCompleteTasksList()
         
         incompleteTableView.delegate = self
         incompleteTableView.dataSource = self
@@ -40,39 +38,6 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
             let tempSwipe = UISwipeGestureRecognizer(target: self, action: #selector(PerformSwipe))
             tempSwipe.direction = gesture
             view.addGestureRecognizer(tempSwipe)
-        }
-        
-//        var tempTask = Task(id: 0, name: "Incomplete Task 1", description: "This is an example of incomplete task.", category: Category.work, status: Status.incomplete, subTask: [Int](), images: [String](), audios: [String](), dueDate: Date(), createdDate: Date())
-//
-//        taskList.append(tempTask)
-//        incompleteTasks.append(tempTask)
-//
-//        tempTask = Task(id: 0, name: "Incomplete Task 2", description: "This is an example of incomplete task.", category: Category.school, status: Status.incomplete, subTask: [Int](), images: [String](), audios: [String](), dueDate: Date(), createdDate: Date())
-//
-//        taskList.append(tempTask)
-//        incompleteTasks.append(tempTask)
-//
-//        tempTask = Task(id: 0, name: "Complete Task 1", description: "This is an example of complete task.", category: Category.groceries, status: Status.complete, subTask: [Int](), images: [String](), audios: [String](), dueDate: Date(), createdDate: Date())
-//
-//        taskList.append(tempTask)
-//        completeTasks.append(tempTask)
-//
-//        tempTask = Task(id: 0, name: "Complete Task 2", description: "This is an example of complete task.", category: Category.shopping, status: Status.complete, subTask: [Int](), images: [String](), audios: [String](), dueDate: Date(), createdDate: Date())
-//
-//        taskList.append(tempTask)
-//        completeTasks.append(tempTask)
-        
-        //clearTaskData()
-        
-        //testing core data
-        //addTask(t: Task(id: 0, name: "Complete 1", description: "This is an example of complete task.", category: Category.school, status: Status.complete, subTask: [1,2], images: ["hello", "world"], audios: ["za", "wurdo"], dueDate: Date(), createdDate: Date()))
-        //addTask(t: Task(id: 0, name: "Incomp 2", description: "This is an example of incomplete task.", category: Category.school, status: Status.incomplete, subTask: [1,2], images: ["hello", "world"], audios: ["za", "wurdo"], dueDate: Date(), createdDate: Date()))
-//
-        loadTasks()
-        print("There are \(String(describing: tasks?.count)) tasks")
-        for t in tasks!{
-            print("Task name is \(t.getName())")
-            print(t.getAudios())
         }
     }
     
@@ -146,93 +111,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         }
     }
     
-    //function to add a task to core data
-    func addTask(t:Task){
-                
-                let newTask = NSEntityDescription.insertNewObject(forEntityName: "TaskEntity", into: managedContext)
-                
-                newTask.setValue(t.getName(), forKey: "name")
-                newTask.setValue(t.getDescription(), forKey: "desc")
-                newTask.setValue(t.getStatus().rawValue, forKey: "status")
-                newTask.setValue(t.getSubTask(), forKey: "subtask")
-                newTask.setValue(t.getImages(), forKey: "images")
-                newTask.setValue(t.getAudios(), forKey: "audios")
-                newTask.setValue(t.getDueDate(), forKey: "dueDate")
-                newTask.setValue(t.getCreatedDate(), forKey: "createdDate")
-                newTask.setValue(t.getCategory().rawValue, forKey: "category")
-
-                do {
-                    try managedContext.save()
-                    print("Record Added!")
-                    //To display an alert box
-                    let alertController = UIAlertController(title: "Message", message: "Task Added!", preferredStyle: .alert)
-                    let OKAction = UIAlertAction(title: "OK", style: .default) {
-                        (action: UIAlertAction!) in
-                    }
-                    alertController.addAction(OKAction)
-                    self.present(alertController, animated: true, completion: nil)
-                } catch
-                let error as NSError {
-                    print("Could not save. \(error),\(error.userInfo)")
-                }
-    }
-    
-    //function to load tasks from core data
-    func loadTasks() {
-            tasks = [Task]()
-            incompleteTasks = [Task]()
-            completeTasks = [Task]()
-            
-            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "TaskEntity")
-            
-            do {
-                        let results = try managedContext.fetch(fetchRequest)
-                        if results is [NSManagedObject] {
-                        for result in (results as! [NSManagedObject]) {
-                            
-                        let name = result.value(forKey: "name") as! String
-                        let descr = result.value(forKey: "desc") as! String
-                            let status = result.value(forKey: "status") as! Status.RawValue
-                        let subtask = result.value(forKey: "subtask") as! [Int]
-                        let images = result.value(forKey: "images") as! [String]
-                        let audios = result.value(forKey: "audios") as! [String]
-                        let dueDate = result.value(forKey: "dueDate") as! Date
-                        let createdDate = result.value(forKey: "createdDate") as! Date
-                            let cat = result.value(forKey: "category") as! Category.RawValue
-                        
-                            tasks?.append(Task(id: 0, name: name, description: descr, category: Category(rawValue: cat)!, status: Status(rawValue: status)!, subTask: subtask, images: images, audios: audios, dueDate: dueDate, createdDate: createdDate))
-                            if(Status(rawValue: status)! == Status.incomplete){
-                                incompleteTasks.append(Task(id: 0, name: name, description: descr, category: Category(rawValue: cat)!, status: Status(rawValue: status)!, subTask: subtask, images: images, audios: audios, dueDate: dueDate, createdDate: createdDate))
-                            }else{
-                                completeTasks.append(Task(id: 0, name: name, description: descr, category: Category(rawValue: cat)!, status: Status(rawValue: status)!, subTask: subtask, images: images, audios: audios, dueDate: dueDate, createdDate: createdDate))
-                            }
-                    }
-                }
-                
-            } catch {
-                print(error)
-            }
-        }
-    
-    // function to delete all tasks from core data
-    func clearTaskData() {
-        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "TaskEntity")
-
-        do {
-            let managedContext = appDelegate.persistentContainer.viewContext
-            let results = try managedContext.fetch(fetchRequest)
-            for result in results {
-                if let managedObject = result as? NSManagedObject {
-                    managedContext.delete(managedObject)
-                }
-            }
-        } catch {
-            print("Error deleting records \(error)")
-        }
-    }
-    
     @IBAction func unwindToMainView(segue: UIStoryboardSegue) {
-        loadTasks()
         DispatchQueue.global(qos: .userInitiated).async {
             DispatchQueue.main.async {
                 self.incompleteTableView.reloadData()
--- task_Amigos_iOS/Core/TaskManager.swift
+++ task_Amigos_iOS/Core/TaskManager.swift
@@ -5,24 +5,118 @@
 //  Created by Daniel Miolan on 1/20/22.
 //
 
+import UIKit
 import Foundation
+import CoreData
 
 class TaskManager {
-    private var taskList: [Task]
-    private var incompleteTasks: [Task]
-    private var completeTasks: [Task]
+    var tasks: [Task]
+    var incompleteTasks: [Task] = [Task]()
+    var completeTasks: [Task] = [Task]()
+    var managedContext: NSManagedObjectContext!
     
     init() {
-        taskList = [Task]()
+        tasks = [Task]()
         incompleteTasks = [Task]()
         completeTasks = [Task]()
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        managedContext = appDelegate.persistentContainer.viewContext
+        
+        loadData()
     }
     
-    func LoadData() {
-        // HERE WE NEED TO EXECUTE THE CODE TO LOAD PERSISTEN DATA
+    func loadData() {
+        tasks = [Task]()
+        incompleteTasks = [Task]()
+        completeTasks = [Task]()
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "TaskEntity")
+        
+        do {
+            let results = try managedContext.fetch(fetchRequest)
+            if results is [NSManagedObject] {
+            for result in (results as! [NSManagedObject]) {
+                let name = result.value(forKey: "name") as! String
+                let descr = result.value(forKey: "desc") as! String
+                let status = result.value(forKey: "status") as! Status.RawValue
+                let subtask = result.value(forKey: "subtask") as! [Int]
+                let images = result.value(forKey: "images") as! [String]
+                let audios = result.value(forKey: "audios") as! [String]
+                let dueDate = result.value(forKey: "dueDate") as! Date
+                let createdDate = result.value(forKey: "createdDate") as! Date
+                let cat = result.value(forKey: "category") as! Category.RawValue
+                let tempTask = Task(id: 0, name: name, description: descr, category: Category(rawValue: cat)!, status: Status(rawValue: status)!, subTask: subtask, images: images, audios: audios, dueDate: dueDate, createdDate: createdDate)
+                
+                    tasks.append(tempTask)
+                    if(Status(rawValue: status)! == Status.incomplete){
+                        incompleteTasks.append(tempTask)
+                    }else{
+                        completeTasks.append(tempTask)
+                    }
+                }
+            }
+        } catch {
+            print(error)
+        }
     }
     
-    func GetTaskByIndex(index: Int) -> Task {
-        return taskList[index]
+    
+    //function to add a task to core data
+    func addTask(task:Task, view: UIViewController){
+            let newTask = NSEntityDescription.insertNewObject(forEntityName: "TaskEntity", into: managedContext)
+            
+            newTask.setValue(task.getName(), forKey: "name")
+            newTask.setValue(task.getDescription(), forKey: "desc")
+            newTask.setValue(task.getStatus().rawValue, forKey: "status")
+            newTask.setValue(task.getSubTask(), forKey: "subtask")
+            newTask.setValue(task.getImages(), forKey: "images")
+            newTask.setValue(task.getAudios(), forKey: "audios")
+            newTask.setValue(task.getDueDate(), forKey: "dueDate")
+            newTask.setValue(task.getCreatedDate(), forKey: "createdDate")
+            newTask.setValue(task.getCategory().rawValue, forKey: "category")
+
+            do {
+                try managedContext.save()
+                print("Record Added!")
+                //To display an alert box
+                let alertController = UIAlertController(title: "Message", message: "Task Added!", preferredStyle: .alert)
+                let OKAction = UIAlertAction(title: "OK", style: .default) {
+                    (action: UIAlertAction!) in
+                }
+                alertController.addAction(OKAction)
+                view.present(alertController, animated: true, completion: nil)
+            } catch
+            let error as NSError {
+                print("Could not save. \(error),\(error.userInfo)")
+            }
+    }
+    
+    // function to delete all tasks from core data
+    func clearTaskData() {
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "TaskEntity")
+
+        do {
+            let results = try managedContext.fetch(fetchRequest)
+            for result in results {
+                if let managedObject = result as? NSManagedObject {
+                    managedContext.delete(managedObject)
+                }
+            }
+        } catch {
+            print("Error deleting records \(error)")
+        }
+    }
+    
+    func getIncompleteTasksList() -> [Task] {
+        return incompleteTasks
+    }
+    
+    func getCompleteTasksList() -> [Task] {
+        return completeTasks
+    }
+    
+    func getTaskByIndex(index: Int) -> Task {
+        return tasks[index]
     }
 }
