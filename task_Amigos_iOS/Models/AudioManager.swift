7b06543e78cae560bad032e397d6901042d6e4ef
simranjit-contact <C0809865@mylambton.ca>
2022-01-28 08:03:25 +0800

    Added Player

--- task_Amigos_iOS/Models/AudioManager.swift
+++ task_Amigos_iOS/Models/AudioManager.swift
@@ -11,6 +11,7 @@ import AVFoundation
 class AudioManager{
     
     private var name: String
+    var player: AVAudioPlayer?
     
     init(name: String) {
         self.name = name
@@ -35,8 +36,32 @@ class AudioManager{
         
     }
     
-    func playAudio(name: String){
-        
+    func playAudio(_name: String){
+        if let player = player, player.isPlaying{
+            //stop playback
+            player.stop()
+        }
+        else{
+            //set up player, and play
+            
+            let urlString = Bundle.main.path(forResource: name, ofType: ".mp3")
+            do{
+                try AVAudioSession.sharedInstance().setMode(.default)
+                try AVAudioSession.sharedInstance().setActive(true, options: .notifyOthersOnDeactivation)
+                
+                guard let urlString = urlString else{
+                    return
+                }
+                player = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: urlString))
+                guard let player = player else{
+                    return
+                }
+                player.play()
+            }
+            catch {
+                print("Something went wrong")
+            }
+        }
         
     }
     
